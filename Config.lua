9dfc0425fcd5214e16abf3afcabea07d9353daa2
gxlfntw <116508831+golfstarnin2372@users.noreply.github.com>
2024-11-02 03:36:31 +0700

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -29,7 +29,7 @@ _G.MainSettings = {
 _G.Fruits_Settings = {
     --['Main_Fruits'] = { 'Buddha-Buddha', 'Dough-Dough' },
     --['Main_Fruits'] = { 'Dark-Dark', 'Magma-Magma', 'Dough-Dough' },
-    ['Main_Fruits'] = { 'ice-ice', 'Magma-Magma', 'Dough-Dough' },
+    ['Main_Fruits'] = { 'Dark-Dark','Ice-Ice', 'Magma-Magma', 'Dough-Dough' },
     ['Select_Fruits'] = { 'Dark-Dark' }
 }
 _G.Quests_Settings = {
